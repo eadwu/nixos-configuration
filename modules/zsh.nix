5ba9b817cbd8212ee0c44d82ad34ab794fbb0afb
Edmund Wu <fangkazuto@gmail.com>
2018-09-22 09:28:46 -0400

    spaceship-prompt: 2018-08-26 -> 2018-09-11

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -7,12 +7,12 @@ let
 
   spaceship-prompt = stdenv.mkDerivation rec {
     name = "spaceship-prompt-${version}";
-    version = "2018-08-26";
+    version = "2018-09-11";
 
     src = pkgs.fetchgit {
       url = "https://github.com/denysdovhan/spaceship-prompt";
-      rev = "63a3611e4bd863f042113459752b365070f0c131";
-      sha256 = "1cl62lfhfpkmbnnlici0pb1cxbg54xxk0v2nxr0h2kimj2cpvp1y";
+      rev = "35ac6cd1e07b1d3fb44d605d628f6e6c4073b7ea";
+      sha256 = "10hqp52c8agxyszwqmrjp6m0v29b0ykqys9ixwl6iawqcwwcwd2c";
     };
 
     dontBuild = true;
