0fdcbd61c1f124083a94034fd4f58bdb9bf852fa
Edmund Wu <fangkazuto@gmail.com>
2018-09-22 22:04:03 -0400

    Convert <overlays> to <nixpkgs-overlays>

--- modules/nix.nix
+++ modules/nix.nix
@@ -36,7 +36,7 @@ in with settings; {
       else [
         "nixpkgs=https://gitlab.com/eadwu/nixpkgs/-/archive/develop/nixpkgs-develop.tar.gz"
         "nixpkgs=https://api.github.com/repos/eadwu/nixpkgs/tarball/develop"
-      ]) ++ lib.optional (builtins.pathExists ../overlays) "overlays=${../overlays}";
+      ]) ++ lib.optional (builtins.pathExists ../overlays) "nixpkgs-overlays=${../overlays}";
   };
 
   nixpkgs = {
@@ -44,9 +44,9 @@ in with settings; {
       allowUnfree = true;
     };
 
-    overlays = if builtins.pathExists <overlays>
-      then import <overlays>
-      else import ../overlays;
+    overlays = if builtins.pathExists <nixpkgs-overlays>
+      then [ (import <nixpkgs-overlays>) ]
+      else [ (import ../overlays) ];
   };
 
   system = {
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,38 +1,36 @@
-[
-  (import ./pkgs/emacs.nix)
-  (import ./pkgs/polybar.nix)
-  (import ./pkgs/suckless.nix)
-  (import ./pkgs/vscode.nix)
+self: super: with self.pkgs; {
+  inherit (import ./pkgs/emacs.nix self super) emacs;
+  inherit (import ./pkgs/polybar.nix self super) polybar;
+  inherit (import ./pkgs/suckless.nix self super) dmenu dwm st;
+  inherit (import ./pkgs/vscode.nix self super) vscode;
 
-  (self: super: with self.pkgs; {
-    ark = super.ark.override {
-      unfreeEnableUnrar = true;
+  ark = super.ark.override {
+    unfreeEnableUnrar = true;
+  };
+
+  capitaine-cursors = super.capitaine-cursors.overrideAttrs (oldAttrs: {
+    src = fetchgit {
+      url = "https://github.com/keeferrourke/capitaine-cursors";
+      rev = "960f9e9011bab281bbcb1355fc45d43295282034";
+      sha256 = "1szxwwj1x87wlvj18jdjxh4dl88hkkq9fp3sl58wcp9jw6iaixrv";
     };
+  });
 
-    capitaine-cursors = super.capitaine-cursors.overrideAttrs (oldAttrs: {
-      src = fetchgit {
-        url = "https://github.com/keeferrourke/capitaine-cursors";
-        rev = "960f9e9011bab281bbcb1355fc45d43295282034";
-        sha256 = "1szxwwj1x87wlvj18jdjxh4dl88hkkq9fp3sl58wcp9jw6iaixrv";
-      };
-    });
+  glava = super.glava.override {
+    enableGlfw = true;
+  };
 
-    glava = super.glava.override {
-      enableGlfw = true;
+  lightdm-enso-os-greeter = super.lightdm-enso-os-greeter.overrideAttrs (oldAttrs: {
+    src = fetchgit {
+      url = "https://github.com/nick92/Enso-OS";
+      rev = "91a064a4dcf469c1325be3806f016434e15da6e0";
+      sha256 = "0y937zxsgdzja17hkr8gpz2yzjzydf90l0y0rsja29jawir8qqm6";
     };
+  });
 
-    lightdm-enso-os-greeter = super.lightdm-enso-os-greeter.overrideAttrs (oldAttrs: {
-      src = fetchgit {
-        url = "https://github.com/nick92/Enso-OS";
-        rev = "91a064a4dcf469c1325be3806f016434e15da6e0";
-        sha256 = "0y937zxsgdzja17hkr8gpz2yzjzydf90l0y0rsja29jawir8qqm6";
-      };
-    });
-
-    ncmpcpp = super.ncmpcpp.override {
-      clockSupport = true;
-      outputsSupport = true;
-      visualizerSupport = true;
-    };
-  })
-]
+  ncmpcpp = super.ncmpcpp.override {
+    clockSupport = true;
+    outputsSupport = true;
+    visualizerSupport = true;
+  };
+}
