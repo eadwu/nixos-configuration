dbf4a76208229febcc2a8444bc898d90edc0b16f
Edmund Wu <fangkazuto@gmail.com>
2018-09-23 10:57:48 -0400

    lightdm-enso-os-greeter: 91a064a -> 2d41840

--- overlays/default.nix
+++ overlays/default.nix
@@ -23,8 +23,8 @@ self: super: with self.pkgs; {
   lightdm-enso-os-greeter = super.lightdm-enso-os-greeter.overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = "https://github.com/nick92/Enso-OS";
-      rev = "91a064a4dcf469c1325be3806f016434e15da6e0";
-      sha256 = "0y937zxsgdzja17hkr8gpz2yzjzydf90l0y0rsja29jawir8qqm6";
+      rev = "2d4184080c644b92b23f7a98e87b81b70b862bca";
+      sha256 = "0157586alivhjjhb6xainkjhw4spps3bqwi4v3cmpmcm1nq5h8f2";
     };
   });
 
