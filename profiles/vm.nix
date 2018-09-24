b4183b8b7f57d20cfbd0f78e2e31e4039d2dcbfc
Edmund Wu <fangkazuto@gmail.com>
2018-09-24 17:54:20 -0400

    Move virtual machine configuration to profiles/vm.nix

---
+++ profiles/vm.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, lib, ... }:
+
+let
+  settings = import ../settings.nix;
+in with settings; {
+  imports =
+    [
+      <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix>
+
+      ../hardware/webcam
+      ../hardware/profiles/intel.nix
+      ../hardware/profiles/laptop.nix
+      ../hardware/profiles/linux.nix
+      # ../modules/xserver/window-manager/bspwm
+      ../modules/xserver/window-manager/dwm
+      ./desktop.nix
+    ];
+
+  virtualbox = {
+    baseImageSize = 64 * 1024;
+    memorySize = 2048;
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  services = {
+    xserver = {
+      displayManager = {
+        lightdm = {
+          greeters = {
+            enso = {
+              enable = lib.mkForce false;
+            };
+
+            gtk = {
+              enable = lib.mkForce true;
+            };
+          };
+        };
+      };
+    };
+  };
+
+  users = {
+    extraUsers = {
+      "${user}" = {
+        hashedPassword = "$6$eZVmFXSPY.7$RkJCABsQErbv/9I2Ovr1ZMw6u.DX/JpGiTXtnXU1mCkQF3fFarKKXPQ2A/Pmy.5/Y7kNIPJndpV5gqSGxmsu51";
+      };
+    };
+  };
+}
