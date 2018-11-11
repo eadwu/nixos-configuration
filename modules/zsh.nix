8dabc6446f3fc5f9fc35ab2cf35b0b53f5e91f72
Edmund Wu <fangkazuto@gmail.com>
2018-11-10 21:53:52 -0500

    Manage theme through oh-my-zsh

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,36 +1,12 @@
 { config, pkgs, ... }:
 
-with import <nixpkgs> { };
-
 let
   settings = import ../settings.nix;
-
-  spaceship-prompt = stdenv.mkDerivation rec {
-    name = "spaceship-prompt-${version}";
-    version = "2018-09-11";
-
-    src = pkgs.fetchgit {
-      url = "https://github.com/denysdovhan/spaceship-prompt";
-      rev = "35ac6cd1e07b1d3fb44d605d628f6e6c4073b7ea";
-      sha256 = "0hwl44qqa97172rndiv5fg59v1rzyr5msx5l17zgqrfhad4jwpwg";
-    };
-
-    dontBuild = true;
-
-    installPhase = ''
-      mkdir -p $out
-
-      cp -r lib $out
-      cp -r sections $out
-      cp spaceship.zsh $out/prompt_spaceship_setup
-    '';
-  };
 in with settings; {
   programs = {
     zsh = {
       enable = true;
       interactiveShellInit = ''
-        fpath+=(${spaceship-prompt})
         setopt histignorespace
 
         bios-upgrade () {
@@ -48,11 +24,13 @@ in with settings; {
           nix-collect-garbage -d
         }
       '';
-      promptInit = ''
-        autoload -U promptinit
-        promptinit
-        prompt spaceship
-      '';
+
+      ohMyZsh = {
+        enable = true;
+        customPkgs = with pkgs; [ spaceship-prompt ];
+        theme = "spaceship";
+      };
+
       shellAliases = {
         "download-audio" = "${pkgs.youtube-dl}/bin/youtube-dl --extract-audio --audio-format mp3";
         "emacs-nox" = "${pkgs.emacs}/bin/emacs --no-window-system";
