19748ff85f3f19ad33336f89f114353589bfbcd2
Edmund Wu <fangkazuto@gmail.com>
2018-11-17 09:38:42 -0500

    Prefer the mq-deadline i/o scheduler

--- hardware/profiles/laptop.nix
+++ hardware/profiles/laptop.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  boot = {
+    kernelParams = [
+      # https://wiki.archlinux.org/index.php/improving_performance#Changing_I/O_scheduler
+      "scsi_mod.use_blk_mq=1"
+    ];
+  };
+
   fileSystems = {
     "/" = {
       options = [
@@ -30,6 +37,8 @@
   services = {
     udev = {
       extraRules = ''
+        # Set scheduler for SSDs
+        ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*|nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
         # Automatically suspend the system at <5%
         SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl suspend"
       '';
