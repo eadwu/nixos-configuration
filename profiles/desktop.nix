9910a8e391ad1d768bfb0d2f18a1c3c5e91cfdf2
Edmund Wu <fangkazuto@gmail.com>
2018-12-27 22:38:52 -0500

    Remove unused args

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 with import <nixpkgs> { };
 
--- profiles/vm.nix
+++ profiles/vm.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 
 with config.nixos.custom; {
   imports =
