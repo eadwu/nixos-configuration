549dae405dd2a9d20ec12d8f3fd8fcc6be03d5c1
Edmund Wu <fangkazuto@gmail.com>
2019-01-23 14:42:09 -0500

    Prefer the official cache server
    Cachix is generally fine, but when down, it causes problems in rebuild

--- modules/nix.nix
+++ modules/nix.nix
@@ -8,24 +8,8 @@ with config.nixos; {
     requireSignedBinaryCaches = true;
     useSandbox = true;
 
-    binaryCaches = [
-      "https://cache.nixos.org/"
-
-      # Cachix
-      "https://emacs.cachix.org"
-      "https://general.cachix.org"
-      "https://qt5.cachix.org"
-      "https://texlive.cachix.org"
-    ];
-
-    binaryCachePublicKeys = [
-      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-
-      "emacs.cachix.org-1:BgvmrJ8yBNHewnYF2wsp+yrPz+Rx7zrLSsevcVrL3Lg="
-      "general.cachix.org-1:fOffqKqBUUQMpjzBfpQR9gPqoQuKSL70A18iI100A6I="
-      "qt5.cachix.org-1:Ka/DtafK6EmdVWthzRpYo+N0j0X6j7gUOIfenczmV7s="
-      "texlive.cachix.org-1:OWd0kb4Wkw8F7T0+sSAPzyOo/pH+NEqIhAWty25oCLk="
-    ];
+    binaryCaches = [ "https://cache.nixos.org/" ];
+    binaryCachePublicKeys = [ "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" ];
 
     extraOptions = ''
       show-trace = true
