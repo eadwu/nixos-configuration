6f45845fd7bf5f241b423b0d149c975d503ec476
Edmund Wu <fangkazuto@gmail.com>
2019-01-27 17:11:08 -0500

    Prefer QEMU over Virtualbox

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,17 +1,8 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   virtualisation = {
+    docker.enable = true;
     libvirtd.enable = true;
-
-    docker = {
-      enable = true;
-    };
-
-    virtualbox = {
-      host = {
-        enable = config.boot.kernelPackages.kernel.version != pkgs.linux_testing.version;
-      };
-    };
   };
 }
