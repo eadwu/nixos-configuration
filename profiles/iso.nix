5a66be0db2f64b90bcd15454325137db3fca6b3e
Edmund Wu <fangkazuto@gmail.com>
2019-02-03 19:58:16 -0500

    Expand iso build
    Include all dependencies needed for a local installation
    Support bcachefs and use unstable zfs

--- profiles/iso.nix
+++ profiles/iso.nix
@@ -7,13 +7,27 @@
       <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel.nix>
     ];
 
-  boot.supportedFilesystems = lib.singleton "bcachefs";
+  boot = {
+    zfs.enableUnstable = true;
+    supportedFilesystems = lib.singleton "bcachefs";
+  };
 
   environment.systemPackages = lib.singleton pkgs.emacs;
 
   hardware.enableRedistributableFirmware = true;
 
   i18n.consoleFont = "latarcyrheb-sun32";
+
+  isoImage = {
+    includeSystemBuildDependencies = true;
+
+    contents = [
+      { source = <nixpkgs>;
+        target = "nixpkgs"; }
+      { source = ./..;
+        target = "nixos-configuration"; }
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 }
