c47ba685c98dd34cbbe07a168831c928c75b1953
Edmund Wu <fangkazuto@gmail.com>
2019-02-03 19:55:45 -0500

    blender-2.80.beta: 690478027bd7 -> 141c6073ca39

--- overlays/pkgs/blender.nix
+++ overlays/pkgs/blender.nix
@@ -8,12 +8,12 @@ in {
     pythonPackages = python36Packages;
   }).overrideAttrs (oldAttrs: rec {
     name = "blender-2.80.beta-${version}";
-    version = "690478027bd7";
+    version = "141c6073ca39";
 
     src = fetchgit {
       url = "https://git.blender.org/blender.git";
       rev = version;
-      sha256 = "0zdlad8bhalmvpy441isr3kgm53jz9lacjk25ap9rgqy8pk38a67";
+      sha256 = "0g02y05hr5vim5g6myy16c1dvx6ah8xbjd1j5jw62qd10yfg4kfp";
     };
 
     cmakeFlags = oldAttrs.cmakeFlags ++ [
