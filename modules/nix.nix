cdc4a587425a2df59590d7d4a25292dfc27e7bd2
Edmund Wu <fangkazuto@gmail.com>
2019-02-03 19:57:31 -0500

    Minor nitpicks

--- modules/nix.nix
+++ modules/nix.nix
@@ -25,12 +25,14 @@ with config.nixos; {
       # builders-use-substitutes = true
     '';
 
-    nixPath = let basePath = builtins.toPath "${settings.system.home}/Downloads"; in [
+    nixPath = let
+      basePath = builtins.toPath "${settings.system.home}/Downloads";
+    in [
       "/etc/nixos"
       "nixos-config=/etc/nixos/configuration.nix"
     ]
       ++ (if builtins.pathExists (builtins.toPath "${basePath}/nixpkgs")
-        then [ "nixpkgs=${basePath}/nixpkgs" ]
+        then lib.singleton "nixpkgs=${basePath}/nixpkgs"
         else [
           "nixpkgs=https://gitlab.com/eadwu/nixpkgs/-/archive/develop/nixpkgs-develop.tar.gz"
           "nixpkgs=https://api.github.com/repos/eadwu/nixpkgs/tarball/develop"
@@ -43,8 +45,8 @@ with config.nixos; {
     config.allowUnfree = true;
 
     overlays = if builtins.pathExists <nixpkgs-overlays>
-      then [ (import <nixpkgs-overlays>) ]
-      else [ (import ../overlays) ];
+      then lib.singleton (import <nixpkgs-overlays>)
+      else lib.singleton (import ../overlays);
   };
 
   programs.ssh.extraConfig = ''
