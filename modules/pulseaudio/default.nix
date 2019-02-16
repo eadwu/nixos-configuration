30312516f273f38f46b6241788c5cb5d6a1b2f7f
Edmund Wu <fangkazuto@gmail.com>
2019-02-15 22:31:43 -0500

    pulseaudio: support extra codecs

--- modules/pulseaudio/default.nix
+++ modules/pulseaudio/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 assert builtins.pathExists ./default.pa;
 
@@ -6,6 +6,7 @@ assert builtins.pathExists ./default.pa;
   hardware.pulseaudio = {
     configFile = ./default.pa;
     enable = true;
+    extraModules = lib.singleton pkgs.pulseaudio-modules-bt;
     package = pkgs.pulseaudioFull;
     support32Bit = true;
   };
