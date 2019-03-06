8904562938bc5ed24ce11c94ef4e895954c49cdc
Edmund Wu <fangkazuto@gmail.com>
2019-03-05 20:00:18 -0500

    Set I/O scheduler based on Arch's udev rules

--- hardware/profiles/laptop.nix
+++ hardware/profiles/laptop.nix
@@ -25,8 +25,12 @@
   powerManagement.enable = true;
 
   services.udev.extraRules = ''
-    # Set scheduler for SSDs
-    ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*|nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+    # Set scheduler for NVMe
+    ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/scheduler}="none"
+    # Set scheduler for SSD and eMMC
+    ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+    # Set scheduler for rotating disks
+    ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
     # Automatically suspend the system at <5%
     SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl suspend"
   '';
