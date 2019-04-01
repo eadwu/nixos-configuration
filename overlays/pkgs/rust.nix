ee7db8d9904f598f8909fd5ebeb112e5a527235d
Edmund Wu <fangkazuto@gmail.com>
2019-03-31 21:18:01 -0400

    rustPlatform: 2019-03-23 -> 2019-03-28

--- overlays/pkgs/rust.nix
+++ overlays/pkgs/rust.nix
@@ -3,7 +3,7 @@ self: super:
 {
   rustPlatform = (import "${builtins.fetchGit {
     url = "https://github.com/mozilla/nixpkgs-mozilla";
-  }}/rust-overlay.nix" self super).rustChannelOf { date = "2019-03-23"; channel = "nightly"; } // {
+  }}/rust-overlay.nix" self super).rustChannelOf { date = "2019-03-28"; channel = "nightly"; } // {
     inherit (super.rustPlatform) buildRustPackage recurseForDerivations;
     rustcSrc = self.pkgs.rustPlatform.rust-src;
   };
