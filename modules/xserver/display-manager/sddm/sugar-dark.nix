386bfc6aeb6825d1080be9e9973719fd92eb074a
Edmund Wu <fangkazuto@gmail.com>
2019-05-27 21:12:53 -0400

    sddm-sugar-dark: set video background

--- modules/xserver/display-manager/sddm/sugar-dark.nix
+++ modules/xserver/display-manager/sddm/sugar-dark.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
@@ -7,6 +7,14 @@
     ];
 
   services.xserver.displayManager.sddm.greeters.sugar-dark = {
+    background = pkgs.requireFile {
+      name = "Stream.2971.mp4";
+      sha256 = "1na6jhqz9zvys7pb185bbjh27cgdgx69zqxf6hjxg0ipycsk8dd8";
+      message = ''
+        Stream.2971.mp4 is missing from /nix/store.
+      '';
+    };
+
     interface.forceHideCompletePassword = true;
   };
 }
