56846e7cd80f3da4134e005d1f737b1994ea66a2
Edmund Wu <fangkazuto@gmail.com>
2019-06-09 19:40:56 -0400

    polybar: unstable-2019-06-02 -> unstable-2019-06-04

--- overlays/pkgs/polybar.nix
+++ overlays/pkgs/polybar.nix
@@ -8,13 +8,13 @@ in {
     mpdSupport = true;
     pulseSupport = true;
   }).overrideAttrs (oldAttrs: {
-    version = "unstable-2019-06-02";
+    version = "unstable-2019-06-04";
 
     src = fetchFromGitHub {
       owner = "polybar";
       repo = "polybar";
-      rev = "5be532c51b24ea8633382c97d9a7ca5b40395d3d";
-      sha256 = "1x78vkcm39015wddwbyzka94xxbkapcshaqvag9diysyykgi83jf";
+      rev = "c650513b67b013aa9dd204a4df65ec5b4f11e88a";
+      sha256 = "06gm1alnihgmaf8bcqsqpfk5zylimjifgj67pwamsrnwc5rj2xxa";
       fetchSubmodules = true;
     };
 
