3fe467865812193463d98e8f232ea2bc7bd2796c
Edmund Wu <fangkazuto@gmail.com>
2019-07-10 09:22:46 -0400

    nvidia: prime: set __GL_ExperimentalPerfStrategy
    Supposedly has better gpu clock boost handling

--- hardware/gpu/nvidia/prime/default.nix
+++ hardware/gpu/nvidia/prime/default.nix
@@ -6,6 +6,12 @@
       ../polyfill.nix
     ];
 
+  environment.variables = {
+    # Experimental environment variable for better gpu clock boost handling
+    # https://devtalk.nvidia.com/default/topic/1048768/linux/if-you-have-gpu-clock-boost-problems-please-try-__gl_experimentalperfstrategy-1/3
+    __GL_ExperimentalPerfStrategy = "1";
+  };
+
   hardware.nvidia = {
     modesetting.enable = true;
 
