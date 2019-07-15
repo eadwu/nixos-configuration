f13bcf270c2e570a89ea500bd9eca986966c4079
Edmund Wu <fangkazuto@gmail.com>
2019-07-15 13:17:11 -0400

    rustPlatform: don't inherit nonexistent attribute

--- overlays/pkgs/rust.nix
+++ overlays/pkgs/rust.nix
@@ -4,7 +4,7 @@ self: super:
   rustPlatform = (import "${builtins.fetchGit {
     url = "https://github.com/mozilla/nixpkgs-mozilla";
   }}/rust-overlay.nix" self super).rustChannelOf { date = "2019-07-15"; channel = "nightly"; } // {
-    inherit (super.rustPlatform) buildRustPackage recurseForDerivations;
+    inherit (super.rustPlatform) buildRustPackage;
     rustcSrc = self.pkgs.rustPlatform.rust-src;
   };
 }
