8495098b117bdd91d56d263afadb5ad059b72b17
Edmund Wu <fangkazuto@gmail.com>
2019-07-16 17:49:09 -0400

    clight: adjust to new syntax and configuration

--- modules/clight.nix
+++ modules/clight.nix
@@ -1,22 +1,25 @@
 { lib, ... }:
 
 {
+  location = {
+    latitude = 40.7;
+    longitude = (-73.8);
+  };
+
   services.clight = {
     enable = lib.mkDefault true;
-    latitude = "40.7";
-    longitude = "-73.8";
 
     temperature = {
       day = 6504;
       night = 2700;
     };
 
-    extraConfig = ''
+    settings = {
       captures = 20;
       gamma_long_transition = true;
-      ac_capture_timeouts = [ 120, 300, 60 ];
-      ac_backlight_regression_points = [ 0.0, 0.12, 0.24, 0.37, 0.51, 0.64, 0.70, 0.82, 0.93, 0.97, 1.0 ];
-      batt_backlight_regression_points = [ 0.0, 0.12, 0.18, 0.28, 0.42, 0.49, 0.54, 0.65, 0.75, 0.78, 0.80 ];
-    '';
+      ac_capture_timeouts = [ 120 300 60 ];
+      ac_backlight_regression_points = [ 0.0 0.12 0.24 0.37 0.51 0.64 0.70 0.82 0.93 0.97 1.0 ];
+      batt_backlight_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
+    };
   };
 }
