e8f763ece7b499bb7da0aa063993253404c0d861
Edmund Wu <fangkazuto@gmail.com>
2019-08-03 13:46:26 -0400

    profiles: linux: reenable watchdog

--- hardware/profiles/linux.nix
+++ hardware/profiles/linux.nix
@@ -21,13 +21,9 @@
     ];
 
     kernelParams = [
-      # https://wiki.archlinux.org/index.php/Improving_performance#Watchdogs
-      "nowatchdog"
-
       # https://wiki.archlinux.org/index.php/Power_management
       # Debugging feature to catch hardware hangs that cause a kernel panic
       # Disabling may cause decrease in power usage
-      "nmi_watchdog=0"
       "snd_hda_intel.power_save=1"
       "vm.dirty_writeback_centisecs=6000"
     ];
