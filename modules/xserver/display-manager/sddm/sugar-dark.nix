953304c1132591c642ec3990a97d95af0adb5842
Edmund Wu <fangkazuto@gmail.com>
2019-08-03 21:08:56 -0400

    sddm: sugar-dark: allow substitutes to be used
    So file can be downloaded from a binary cache
    See https://github.com/NixOS/nixpkgs/pull/42637

--- modules/xserver/display-manager/sddm/sugar-dark.nix
+++ modules/xserver/display-manager/sddm/sugar-dark.nix
@@ -7,13 +7,11 @@
     ];
 
   services.xserver.displayManager.sddm.greeters.sugar-dark = {
-    background = pkgs.requireFile {
+    background = (pkgs.requireFile rec {
       name = "video-bg.3e78e808.mp4";
       sha256 = "0c7nvbpbigxcjgqd93038lvkba4z9q0g21jkhr4qwqj0alq026zh";
-      message = ''
-        video-bg.3e78e808.mp4 is missing from /nix/store.
-      '';
-    };
+      message = "${name} is missing from NIX_STORE.";
+    }).overrideAttrs(_: { allowSubstitutes = true; });
 
     interface.forceHideCompletePassword = true;
   };
