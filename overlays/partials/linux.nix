a5e1ebd84b0c9684ce1fbfd4df74e69948608194
Edmund Wu <fangkazuto@gmail.com>
2019-08-16 14:39:44 -0400

    linux_rpi: 4.19.65 -> 4.19.66

--- overlays/partials/linux.nix
+++ overlays/partials/linux.nix
@@ -1,20 +1,20 @@
 self: super:
 
 with self.pkgs; {
-  linux_rpi_4_19 = super.linux_rpi.override {
+  linux_rpi_4_19 = (super.linux_rpi.override {
     argsOverride = rec {
-      version = "4.19.65";
+      version = "4.19.66";
       modDirVersion = with lib; concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
-
-      src = builtins.fetchTarball {
-        url = "https://github.com/raspberrypi/linux/archive/ea2c11a187c0e248343452846457b94715e04969.tar.gz";
-      };
     };
-  };
+  }).overrideDerivation (_: {
+    src = builtins.fetchTarball {
+      url = "https://github.com/raspberrypi/linux/archive/fc5826fb999e0b32900d1f487e90c27a92010214.tar.gz";
+    };
+  });
 
   raspberrypifw = super.raspberrypifw.overrideAttrs (_: {
     src = builtins.fetchTarball {
-      url = "https://github.com/raspberrypi/firmware/archive/66bafab005569e3eb92ec54cd3efeee3da338738.tar.gz";
+      url = "https://github.com/raspberrypi/firmware/archive/b42496fd290dbe8cea47b230eb1782641329a24d.tar.gz";
     };
   });
 }
