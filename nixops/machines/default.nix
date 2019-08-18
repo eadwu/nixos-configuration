c58ea42821d72a287fdb79c9afc7fa060159edaf
Edmund Wu <fangkazuto@gmail.com>
2019-08-17 21:17:43 -0400

    nixops: ensure nixpkgs.system is correct

--- nixops/machines/default.nix
+++ nixops/machines/default.nix
@@ -3,6 +3,8 @@
 {
   imports = [ <nixpkgs/nixos/modules/profiles/hardened.nix> ];
 
+  nixpkgs.system = builtins.currentSystem;
+
   boot.kernelPackages = pkgs.linuxPackages_latest_hardened;
   environment.memoryAllocator.provider = "libc";
   security.allowUserNamespaces = true;
