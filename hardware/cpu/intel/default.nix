7f508fc4ccc495013cc5d991328dd07212bbbfed
Edmund Wu <fangkazuto@gmail.com>
2019-09-27 15:42:28 -0400

    hardware: cpu: intel: include intel-ocl
    Provides an OpenCL device for the CPU

--- hardware/cpu/intel/default.nix
+++ hardware/cpu/intel/default.nix
@@ -16,11 +16,10 @@
     "intel_pstate=passive"
   ];
 
-  environment.systemPackages = with pkgs; [
-    pcm
-  ];
+  environment.systemPackages = with pkgs; [ pcm ];
 
   hardware.cpu.intel.updateMicrocode = true;
+  hardware.opengl.extraPackages = with pkgs; [ intel-ocl ];
 
   services.undervolt.enable = lib.mkDefault true;
 }
