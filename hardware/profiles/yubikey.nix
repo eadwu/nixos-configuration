3a713ce831e66bea595255e76f9a02f3ead703c8
Edmund Wu <fangkazuto@gmail.com>
2019-10-22 17:32:19 -0400

    hardware: profiles: yubikey: use separate files for multiple devices

--- hardware/profiles/yubikey.nix
+++ hardware/profiles/yubikey.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  yubikey-config = {
+  yubikey-config = path: {
     twoFactor = true;
     slot = 2;
     saltLength = 64;
@@ -12,15 +12,14 @@ let
     storage = {
       device = config.fileSystems."/boot".device;
       fsType = "vfat";
-      path = "/crypt-storage/default";
-    };
+    } // { inherit path; };
   };
 in {
   boot.initrd.kernelModules = [ "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
   boot.initrd.luks.yubikeySupport = true;
 
-  boot.initrd.luks.devices."cryptroot".yubikey = yubikey-config;
-  boot.initrd.luks.devices."cryptswap".yubikey = yubikey-config;
+  boot.initrd.luks.devices."cryptroot".yubikey = yubikey-config "/crypt-storage/cryptroot";
+  boot.initrd.luks.devices."cryptswap".yubikey = yubikey-config "/crypt-storage/cryptswap";
 
   environment.systemPackages = with pkgs; [ pam_u2f yubikey-personalization ];
 
