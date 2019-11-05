7e6224bf1af7830eb8ad035b22ee3cac90c45c9d
Edmund Wu <fangkazuto@gmail.com>
2019-11-04 21:03:35 -0500

    sddm: sugar-dark: DASH_1080.mp4 -> Scissors-Congruence_2880x1620-Lede.mp4

--- modules/xserver/display-manager/sddm/sugar-dark.nix
+++ modules/xserver/display-manager/sddm/sugar-dark.nix
@@ -9,8 +9,8 @@
   services.xserver.displayManager.sddm.greeters.sugar-dark = {
     background = (
       pkgs.requireFile rec {
-        name = "DASH_1080.mp4";
-        sha256 = "0aa8zmqa12agjs4482z4kl2zfp5s6fjvdrfp2jg946i027jzdcds";
+        name = "Scissors-Congruence_2880x1620-Lede.mp4";
+        sha256 = "1dwxd0hx2mbxr0sd0yjpvygy2l0fpv135is2qgrcbqs22jx57f7y";
         message = "${name} is missing from NIX_STORE.";
       }
     ).overrideAttrs (_: { allowSubstitutes = true; });
