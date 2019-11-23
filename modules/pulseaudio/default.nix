d6cf17a9a5a84d4b934d4b903adeb8747682b41f
Edmund Wu <fangkazuto@gmail.com>
2019-11-22 21:20:22 -0500

    pulseaudio: allow tcp from local client networks

--- modules/pulseaudio/default.nix
+++ modules/pulseaudio/default.nix
@@ -8,5 +8,10 @@ assert builtins.pathExists ./default.pa;
     enable = true;
     extraModules = [ pkgs.pulseaudio-modules-bt ];
     package = pkgs.pulseaudioFull;
+
+    tcp = {
+      enable = true;
+      anonymousClients.allowedIpRanges = [ "127.0.0.1" "192.168.1.0/24" ];
+    };
   };
 }
