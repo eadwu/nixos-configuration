b2817c76123ad42eeb6cd42508b8e76822ddac23
Edmund Wu <fangkazuto@gmail.com>
2020-03-15 21:01:44 -0400

    emacs: update wakatime api-key

--- modules/home-manager/emacs/modules/tools/wakatime.el
+++ modules/home-manager/emacs/modules/tools/wakatime.el
@@ -1,7 +1,7 @@
 (use-package wakatime-mode
   :diminish
   :custom
-  (wakatime-api-key "83e9edbb-e265-4cdd-b39b-5442e77c806b")
+  (wakatime-api-key "1b112c4e-d322-4252-98d8-acb8addda65d")
   (wakatime-cli-path "wakatime")
   (wakatime-python-bin "")
   :init (global-wakatime-mode)
