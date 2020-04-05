eb9e1e197f23ff279742544afd479a77e38a7667
Edmund Wu <fangkazuto@gmail.com>
2020-04-05 14:15:03 -0400

    hardware/gpu/nvidia/disable: fix udev rule for nvidia

--- hardware/gpu/nvidia/disable/default.nix
+++ hardware/gpu/nvidia/disable/default.nix
@@ -14,7 +14,7 @@
   services = {
     udev.extraRules = ''
       # Runtime PM for PCI Device NVIDIA Corporation GP107M [GeForce GTX 1050 Ti Mobile]
-      ACTION=="add", SUBSYSTEMS=="pci", ATTRS{device}=="0x1901", ATTRS{vendor}=="0x8086", TEST=="power/control", ATTR{power/control}:="auto"
+      ACTION=="add", SUBSYSTEMS=="pci", ATTRS{device}=="0x1901", ATTRS{vendor}=="0x8086", TEST=="power/control", ATTR{power/control}="auto"
     '';
   };
 }
