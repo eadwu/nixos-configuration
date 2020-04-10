e217a31f7861f3f5363dc037803bbf2eea8a1cef
Edmund Wu <fangkazuto@gmail.com>
2020-04-10 17:30:28 -0400

    overlays: use picom from flake:custom

--- overlays/default.nix
+++ overlays/default.nix
@@ -41,20 +41,6 @@ in
       ) { p = p: with p; [ vbox libvirtd ]; }
     ).build.${stdenv.hostPlatform.system};
 
-    picom = super.picom.overrideAttrs (_: rec {
-      version = "2020-04-01";
-
-      src = fetchFromGitHub {
-        owner = "yshui";
-        repo = "picom";
-        rev = "85086bc9843f408b761580357d4f88b5ea626ada";
-        sha256 = "sha256-uI5DSQ/ea4usRkYD/0mIzFrgb5o8RUfoQfhXflFH05w=";
-        fetchSubmodules = true;
-      };
-
-      patches = [ ../patches/picom/dual_kawase.patch ];
-    });
-
     polybar = super.polybar.override {
       githubSupport = true;
       mpdSupport = true;
