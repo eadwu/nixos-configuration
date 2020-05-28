119e8db953a783984f36d0e2cc35b37e981fdb67
Edmund Wu <fangkazuto@gmail.com>
2020-05-28 12:05:10 -0400

    modules/tlp: update config
    Use default CPU_MAX_PERF_ON_BAT
    Pull in default SCHED_POWERSAVE_ON_{AC,BAT}
    CUP_HWP_ON_* -> CPU_ENERGY_PERF_POLICY_ON_*

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -4,19 +4,27 @@
   services.tlp = {
     enable = lib.mkDefault true;
     extraConfig = ''
-      CPU_HWP_ON_AC=balance_performance
-      CPU_HWP_ON_BAT=balance_power
-
+      # Set CPU governors
       CPU_SCALING_GOVERNOR_ON_AC=performance
       CPU_SCALING_GOVERNOR_ON_BAT=powersave
 
+      # Disable Turbo [Boost|Core] on battery
+      CPU_BOOST_ON_AC=1
+      CPU_BOOST_ON_BAT=0
+
+      # Intel CPU HWP hints
+      CPU_ENERGY_PERF_POLICY_ON_AC=balance_performance
+      CPU_ENERGY_PERF_POLICY_ON_BAT=balance_power
+
+      # Set CPU Frequency constraints
       CPU_MIN_PERF_ON_AC=0
       CPU_MAX_PERF_ON_AC=100
       CPU_MIN_PERF_ON_BAT=0
-      CPU_MAX_PERF_ON_BAT=40
+      CPU_MAX_PERF_ON_BAT=30
 
-      CPU_BOOST_ON_AC=1
-      CPU_BOOST_ON_BAT=0
+      # Use minimum amount of cores on battery
+      SCHED_POWERSAVE_ON_AC=0
+      SCHED_POWERSAVE_ON_BAT=1
     '';
   };
 }
