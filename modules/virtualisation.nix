a99acd376e3141a4a9052ea1d78189b69440dc6c
Edmund Wu <fangkazuto@gmail.com>
2020-05-28 21:04:38 -0400

    modules/virtualisation: drop scream
    Audio can be received with a passthrough through pulseaudio's socket

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -12,7 +12,6 @@
 
   environment.systemPackages = with pkgs; [
     swtpm
-    scream-receivers
     looking-glass-client
   ];
 
@@ -20,18 +19,6 @@
   virtualisation.kvmgt.enable = lib.mkDefault true;
 
   systemd.tmpfiles.rules = [
-    "f /dev/shm/scream        0660 root kvm -"
     "f /dev/shm/looking-glass 0660 root kvm -"
   ];
-
-  systemd.user.services.scream-ivshmem = {
-    enable = true;
-    description = "Scream IVSHMEM";
-    serviceConfig = {
-      ExecStart = "${pkgs.scream-receivers}/bin/scream-ivshmem-pulse /dev/shm/scream";
-      Restart = "always";
-    };
-    wantedBy = [ "multi-user.target" ];
-    requires = [ "pulseaudio.service" ];
-  };
 }
