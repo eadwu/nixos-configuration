d2544756d28fef87078d32efde99c8d6abdd4434
Edmund Wu <fangkazuto@gmail.com>
2020-05-28 21:13:09 -0400

    modules/virtualisation: fix memlock constraint for @kvm

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -18,6 +18,15 @@
   virtualisation.docker.enable = true;
   virtualisation.kvmgt.enable = lib.mkDefault true;
 
+  # Qemu needs more than 64KB of memory to communicate with Direct Memory Access (DMA) with the VFIO GPU
+  # Should generally be around the amount of memory allocated for the VM
+  security.pam.loginLimits = [
+    { domain = "@kvm";
+      type = "-";
+      item = "memlock";
+      value = "unlimited"; }
+  ];
+
   systemd.tmpfiles.rules = [
     "f /dev/shm/looking-glass 0660 root kvm -"
   ];
