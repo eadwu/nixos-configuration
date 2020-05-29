8680802072eb28fe50af98f08a626e8b3b3b367a
Edmund Wu <fangkazuto@gmail.com>
2020-05-28 21:14:03 -0400

    hardware/cpu/intel: enable more KVM options

--- hardware/cpu/intel/default.nix
+++ hardware/cpu/intel/default.nix
@@ -18,6 +18,12 @@
   boot.kernelParams = [
     "intel_iommu=on"
     "iommu=pt"
+
+    "kvm_intel.nested=1"
+    "kvm_intel.enable_apicv=1"
+    # https://www.linux-kvm.org/images/e/e9/Kvm-forum-2013-nested-virtualization-shadow-turtles.pdf
+    "kvm_intel.enlightened_vmcs=1"
+    "kvm_intel.enable_shadow_vmcs=1"
   ];
 
   environment.systemPackages = with pkgs; [ pcm config.boot.kernelPackages.intel-speed-select ];
