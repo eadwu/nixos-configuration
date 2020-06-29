c18920f6691b6097c631f2fe83062cc693fc6f66
Edmund Wu <fangkazuto@gmail.com>
2020-06-29 00:47:30 -0400

    home-manager/mpv: update anime4k shaders

--- home-manager/mpv.nix
+++ home-manager/mpv.nix
@@ -2,15 +2,15 @@
 
 let
   anime4k = pkgs.fetchzip {
-    url = "https://github.com/bloc97/Anime4K/releases/download/v1.0-RC2/Anime4K_GLSL_v1.0RC2.zip";
-    sha256 = "0hdyzd1qbd0p28kq2a9rfgmpdzgba40dwvl01dccb47zclgx0xq5";
+    url = "https://github.com/bloc97/Anime4K/releases/download/3.1/Anime4K_v3.1.zip";
+    sha256 = "11j1x40kfigr76d2a97pp3q78pz6b2vxfmic40zdq0zrwdwxzgbg";
     stripRoot = false;
   };
 in
 {
   home.packages = [ pkgs.mpv ];
-  xdg.configFile = {
-    "mpv/config".source = ./mpv/config;
-    "mpv/shaders/Anime4K_Adaptive.glsl".source = "${anime4k}/Anime4K_Adaptive_v1.0RC2.glsl";
+  xdg.configFile."mpv/config".source = pkgs.substituteAll {
+    src = ./mpv/config;
+    inherit anime4k;
   };
 }
