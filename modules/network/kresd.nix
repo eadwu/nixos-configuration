27e17038a3b72b681716831006756b4dd5b9cf1f
Edmund Wu <fangkazuto@gmail.com>
2020-07-10 16:19:31 -0400

    modules/network/kresd: improve configuration
    Respect rules in /etc/hosts
    Change cache size to 1G
    Use a local file for DNSSEC validation
    Split DNS query between Cloudflare and Quad9
    Use a local certificate for DNS-over-TLS

--- modules/network/kresd.nix
+++ modules/network/kresd.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking.nameservers = [ "127.0.0.1" ];
@@ -7,31 +7,39 @@
     enable = true;
     extraConfig = ''
       modules = {
-        'stats',
-        'policy',
-        'predict',
-        'serve_stale < cache'
+        'hints',
+        'stats', -- identify usage patterns and preemptively refresh expired queries
+        'policy', -- manipulate request handling
+        'predict', -- works together with stats
+        'serve_stale < cache', -- allows expired entries to be served from the cache
+        'workarounds < iterate' -- contains a set of hotfixes to ensure compatibility
       }
 
+      -- Respect /etc/hosts
+      hints.add_hosts()
       -- Cache size
-      cache.size = 150 * MB
+      cache.size = 1024 * MB
+      -- Enable DNSSEC validation
+      trust_anchors.add_file('${pkgs.dns-root-data}/root.key', true)
       -- Prefetch learning (20-minute blocks over 24 hours)
-      predict.config({
-        window = 20,
-        period = 18 * (60 / 15)
-      })
+      predict.config({ window = 20, period = 18 * (60 / 15) })
 
-      -- Forward queries
-      policy.add(policy.all(policy.TLS_FORWARD({
-        { '1.1.1.1', hostname = 'cloudflare-dns.com'},
-        { '1.0.0.1', hostname = 'cloudflare-dns.com'},
-        -- { '2606:4700:4700::1111', hostname = 'cloudflare-dns.com'},
-        -- { '2606:4700:4700::1001', hostname = 'cloudflare-dns.com'},
-        { '9.9.9.9', hostname = 'dns.quad9.net'},
-        { '149.112.112.112', hostname = 'dns.quad9.net'}
-        -- { '2620:fe::fe', hostname = 'dns.quad9.net'},
-        -- { '2620:fe::9', hostname = 'dns.quad9.net'}
-      })))
+      -- Forward queries through Cloudflare and Quad9
+      policy.add(policy.slice(
+        policy.slice_randomize_psl(),
+        policy.TLS_FORWARD({
+          { '2606:4700:4700::1111', hostname = 'cloudflare-dns.com', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '2606:4700:4700::1001', hostname = 'cloudflare-dns.com', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '1.1.1.1', hostname = 'cloudflare-dns.com', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '1.0.0.1', hostname = 'cloudflare-dns.com', ca_file = '/etc/ssl/certs/ca-certificates.crt' }
+        }),
+        policy.TLS_FORWARD({
+          { '2620:fe::fe', hostname = 'dns.quad9.net', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '2620:fe::9', hostname = 'dns.quad9.net', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '9.9.9.9', hostname = 'dns.quad9.net', ca_file = '/etc/ssl/certs/ca-certificates.crt' },
+          { '149.112.112.112', hostname = 'dns.quad9.net', ca_file = '/etc/ssl/certs/ca-certificates.crt' }
+        })
+      ))
     '';
   };
 }
