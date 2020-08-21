2cbb3b985769612297a58cb0eed7d57edbacb395
Edmund Wu <fangkazuto@gmail.com>
2020-08-21 16:47:41 -0400

    modules/monitoring/prometheus: init

--- modules/monitoring/default.nix
+++ modules/monitoring/default.nix
@@ -4,5 +4,6 @@
   imports =
     [
       ./netdata.nix
+      ./prometheus.nix
     ];
 }
---
+++ modules/monitoring/prometheus.nix
@@ -0,0 +1,57 @@
+{ config, ... }:
+
+{
+  services.prometheus = {
+    enable = true;
+    listenAddress = "127.0.0.1:9090";
+
+    globalConfig = {
+      scrape_interval = "1s";
+      evaluation_interval = "1s";
+    };
+
+    scrapeConfigs = [
+      ({
+        job_name = "prometheus";
+        static_configs = [ { targets = [ config.services.prometheus.listenAddress ]; } ];
+      })
+
+      ({
+        job_name = "postgres";
+        static_configs = [ { targets = [ "localhost:9187" ]; } ];
+      })
+
+      ({
+        job_name = "node";
+        static_configs = [ { targets = [ "localhost:9100" ]; } ];
+      })
+    ];
+  };
+
+  services.prometheus.exporters.postgres = {
+    enable = true;
+    openFirewall = false;
+    listenAddress = "127.0.0.1";
+    runAsLocalSuperUser = true;
+  };
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    openFirewall = false;
+    listenAddress = "127.0.0.1";
+    enabledCollectors = [
+      "buddyinfo"
+      "interrupts"
+      "ksmd"
+      "logind"
+      "mountstats"
+      "processes"
+      "qdisc"
+      "systemd"
+      "wifi"
+    ];
+    disabledCollectors = [
+      "textfile"
+    ];
+  };
+}
