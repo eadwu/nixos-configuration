712ae326520804d4bbe0c525760ee6005f9e4349
Edmund Wu <fangkazuto@gmail.com>
2020-08-21 16:48:32 -0400

    modules/monitoring/netdata: integrate with prometheus

--- modules/monitoring/netdata.nix
+++ modules/monitoring/netdata.nix
@@ -2,4 +2,14 @@
 
 {
   services.netdata.enable = true;
+
+  # Prometheus Integration
+  services.prometheus.scrapeConfigs = [
+    ({
+      job_name = "netdata";
+      honor_labels = true;
+      metrics_path = "/api/v1/allmetrics";
+      static_configs = [ { targets = [ "localhost:19999" ]; } ];
+    })
+  ];
 }
