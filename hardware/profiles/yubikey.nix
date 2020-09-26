94a8cff6b212d2a4279394f406f0e938153e1258
Edmund Wu <fangkazuto@gmail.com>
2020-09-25 22:08:12 -0400

    profiles/yubikey: update algorithms used
    Haven't had any problems with weird passwords using these

--- hardware/profiles/yubikey.nix
+++ hardware/profiles/yubikey.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 let
-  yubikey-config = path: {
+  yubikey-config = path: rec {
     twoFactor = true;
     slot = 2;
     saltLength = 64;
@@ -12,6 +12,23 @@ let
       device = config.fileSystems."/boot".device;
       fsType = "vfat";
     } // { inherit path; };
+
+    algo = {
+      digest = {
+        command = "argon2 $response -id -r -p 6 -t $iterations -l ${toString keyLength}";
+        deps = ''
+          copy_bin_and_libs ${pkgs.libargon2}/bin/argon2
+        '';
+      };
+
+      challenge = {
+        raw = true;
+        command = "b3sum --raw --length ${toString keyLength}";
+        deps = ''
+          copy_bin_and_libs ${pkgs.b3sum}/bin/b3sum
+        '';
+      };
+    };
   };
 in
 {
