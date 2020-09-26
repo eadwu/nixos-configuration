84469b5828ab81c6fb680c551ef2e06efdc30371
Edmund Wu <fangkazuto@gmail.com>
2020-09-25 22:06:13 -0400

    modules/clight: fix settings option
    Turns out the configuration format was updated sometime along the way

--- modules/clight.nix
+++ modules/clight.nix
@@ -15,13 +15,17 @@
     };
 
     settings = {
-      captures = 16;
-      screen_samples = 20;
-      screen_contrib = 0.2;
-      gamma_long_transition = true;
-      ac_capture_timeouts = [ 120 300 60 ];
-      ac_backlight_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
-      batt_backlight_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
+      backlight.ac_timeouts = [ 120 300 60 ];
+      backlight.pause_on_lid_closed = true;
+
+      sensor.ac_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
+      sensor.batt_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
+      sensor.captures = 16;
+
+      gamma.long_transition = true;
+
+      screen.contrib = 0.2;
+      screen.num_samples = 20;
     };
   };
 }
