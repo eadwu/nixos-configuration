f13b1f09061fe74844f92d66209b5517902bf1da
Edmund Wu <fangkazuto@gmail.com>
2020-10-02 19:26:24 -0400

    modules/clight: update configuration
    Fix sensor.captures configuration
    Enable verbose output

--- modules/clight.nix
+++ modules/clight.nix
@@ -15,12 +15,14 @@
     };
 
     settings = {
+      verbose = true;
+
       backlight.ac_timeouts = [ 120 300 60 ];
       backlight.pause_on_lid_closed = true;
 
       sensor.ac_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
       sensor.batt_regression_points = [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.80 ];
-      sensor.captures = 16;
+      sensor.captures = [ 16 16 ];
 
       gamma.long_transition = true;
 
