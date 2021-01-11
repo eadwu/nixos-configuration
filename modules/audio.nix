4eec8851979d035e7ab15262006f502dc2cdf9f4
Edmund Wu <fangkazuto@gmail.com>
2021-01-10 22:32:04 -0500

    modules/audio: include some bluetooth adjustments
    Don't quite recall what exactly the bluez configuration is for, though it looks fine

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   imports =
@@ -6,4 +6,24 @@
       ./audio/pipewire.nix
       # ./audio/pulseaudio.nix
     ];
+
+  boot.kernelModules = [ "uinput" ]; # AVRCP protocol support/compatibility for input device
+  systemd.services.bluetooth.serviceConfig.ExecStart = [ "" "${config.hardware.bluetooth.package}/libexec/bluetooth/bluetoothd --noplugin=sap" ];
+
+  hardware.bluetooth.config = {
+    General = {
+      MultiProfile = "multiple";
+      FastConnectable = true;
+      Privacy = "device";
+    };
+
+    GATT = {
+      KeySize = 16;
+    };
+
+    AVDTP = {
+      SessionMode = "ertm";
+      StreamMode = "streaming";
+    };
+  };
 }
