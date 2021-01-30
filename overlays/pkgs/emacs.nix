35163fd987d78d7c240228f8afdbb21473f231e9
Edmund Wu <fangkazuto@gmail.com>
2021-01-30 14:21:31 -0500

    emacs: fix composability of _aliases subset

--- overlays/pkgs/emacs.nix
+++ overlays/pkgs/emacs.nix
@@ -3,53 +3,56 @@ final: prev:
 with final.pkgs;
 
 {
-  _aliases.emacs = (emacsPackagesFor emacsUnstable).emacsWithPackages (
-    epkgs: (
-      with epkgs.elpaPackages; [
-        auctex
-      ]
-    ) ++ (
-      with epkgs.melpaPackages; [
-        ccls
-        flycheck-inline
-        wakatime-mode
-      ]
-    ) ++ (
-      with epkgs.melpaStablePackages; [
-        avy
-        beacon
-        company
-        company-lsp
-        company-web
-        counsel
-        # cquery # cquery has been removed because it is abandoned by upstream. Consider switching to clangd or ccls instead.
-        doom-themes
-        diminish # needed for use-package?
-        ein
-        elpy
-        emmet-mode
-        flycheck
-        hydra
-        interleave
-        ivy-rich
-        js2-mode
-        lsp-java
-        lsp-mode
-        lsp-ui
-        magit
-        mmm-mode
-        nix-buffer
-        nix-mode
-        pandoc-mode
-        projectile
-        spaceline
-        tide
-        use-package
-      ]
-    ) ++ (
-      with epkgs.orgPackages; [
-        org-plus-contrib
-      ]
-    )
-  );
+  _aliases = (prev._aliases or { }) // {
+    emacs = (emacsPackagesFor emacsUnstable).emacsWithPackages
+      (
+        epkgs: (
+          with epkgs.elpaPackages; [
+            auctex
+          ]
+        ) ++ (
+          with epkgs.melpaPackages; [
+            ccls
+            flycheck-inline
+            wakatime-mode
+          ]
+        ) ++ (
+          with epkgs.melpaStablePackages; [
+            avy
+            beacon
+            company
+            company-lsp
+            company-web
+            counsel
+            # cquery # cquery has been removed because it is abandoned by upstream. Consider switching to clangd or ccls instead.
+            doom-themes
+            diminish # needed for use-package?
+            ein
+            elpy
+            emmet-mode
+            flycheck
+            hydra
+            interleave
+            ivy-rich
+            js2-mode
+            lsp-java
+            lsp-mode
+            lsp-ui
+            magit
+            mmm-mode
+            nix-buffer
+            nix-mode
+            pandoc-mode
+            projectile
+            spaceline
+            tide
+            use-package
+          ]
+        ) ++ (
+          with epkgs.orgPackages; [
+            org-plus-contrib
+          ]
+        )
+      );
+  };
 }
