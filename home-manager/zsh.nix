a5ea558734500e8684cac1dccceee4faeaaeeb8e
Edmund Wu <fangkazuto@gmail.com>
2021-05-30 23:45:20 -0400

    home-manager/zsh: define nix-index-update

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -56,6 +56,14 @@
           docker push "''${DOCKER_ID_USER}/$1"
         }
 
+        nix-index-update () {
+          tag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --tags --sort='v:refname' \
+            https://github.com/Mic92/nix-index-database | awk 'END {match($2, /([^/]+)$/, m); print m[0]}')
+
+          curl -L "https://github.com/Mic92/nix-index-database/releases/download/$tag/files" -o $XDG_RUNTIME_DIR/files-$tag
+          mv $XDG_RUNTIME_DIR/files-$tag $HOME/.cache/nix-index/files
+        }
+
         nix-upload-system () {
           local errno
           local outLink
