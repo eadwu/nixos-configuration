f01ede67c181b1f55e6b7495f05a3736bb0caaf7
Edmund Wu <fangkazuto@gmail.com>
2022-06-14 17:37:38 -0700

    home-manager/zsh: nix-index-update: fix link

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -60,7 +60,7 @@
           tag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --tags --sort='v:refname' \
             https://github.com/Mic92/nix-index-database | awk 'END {match($2, /([^/]+)$/, m); print m[0]}')
 
-          curl -L "https://github.com/Mic92/nix-index-database/releases/download/$tag/files" -o $XDG_RUNTIME_DIR/files-$tag
+          curl -L "https://github.com/Mic92/nix-index-database/releases/download/$tag/index-x86_64-linux" -o $XDG_RUNTIME_DIR/files-$tag
           mv $XDG_RUNTIME_DIR/files-$tag $HOME/.cache/nix-index/files
         }
 
